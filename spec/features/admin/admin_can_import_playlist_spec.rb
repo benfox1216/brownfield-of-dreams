bbdb36b093cbb73cc60736f98898ba8ce8d8ed07
Andrew Reid <areid.sj@gmail.com>
2020-05-06 16:32:37 -0600

    Add test to validate video creation and link to tutorial

--- spec/features/admin/admin_can_import_playlist_spec.rb
+++ spec/features/admin/admin_can_import_playlist_spec.rb
@@ -24,4 +24,17 @@ describe "An Admin can import a playlist" do
 
     expect(current_path).to eq("/tutorials/#{tutorial.id}")
   end
+
+  scenario "and import should create video records linked to the tutorial" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/admin/tutorials/new'
+    click_link 'Import YouTube Playlist'
+    fill_in 'Title', with: 'Animals Making Football Predictions'
+    fill_in 'Description', with: "Yeah. It's exactly what it sounds like. What a world we live in."
+    fill_in 'Playlist', with: 'PLbpi6ZahtOH5NpWaK0akzu0Pgley4eEXH'
+    click_on 'Save'
+
+    expect(Video.last.tutorial_id).to eq(Tutorial.last.id)
+  end
 end
