b7918fdc8d4ef7b4a026b193c79010ee612507b9
Andrew Reid <areid.sj@gmail.com>
2020-05-07 15:40:57 -0600

    Remove now unneccessary helper methods from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   helper_method :find_bookmark
   helper_method :list_tags
   helper_method :tutorial_name
-  helper_method :display_repos
-  helper_method :display_follow
 
   add_flash_types :success
 
