1bd63a47e5a5ab40c97021e77ee9afe8b8e9999f
Andrew Reid <areid.sj@gmail.com>
2020-05-07 15:19:34 -0600

    Refactor tests to stub new methods

--- spec/features/user/user_can_see_github_repositories_spec.rb
+++ spec/features/user/user_can_see_github_repositories_spec.rb
@@ -28,18 +28,19 @@ describe 'A registered user' do
     expect(page).to_not have_css(".repos")
   end
 
-  xit 'can see a list of 5 repos in the section' do
+  it 'can see a list of 5 repos in the section' do
     user = create(:user)
     user.update(token: "Not_a_real_token")
-    stubbed_repos = []
-    stubbed_repos << Repo.new("Google", "www.google.com")
-    stubbed_repos << Repo.new("Yahoo", "www.yahoo.com")
-    stubbed_repos << Repo.new("Bing", "www.bing.com")
-    stubbed_repos << Repo.new("Alta Vista", "www.altavista.com")
-    stubbed_repos << Repo.new("Search", "www.search.com")
-    stubbed_repos << Repo.new("Imgur", "www.imgur.com")
+    stubbed_repos = [{"id"=>1, "name"=>"Google", "html_url"=>"www.google.com"},
+                    {"id"=>2, "name"=>"Yahoo", "html_url"=>"www.yahoo.com"},
+                    {"id"=>3, "name"=>"Bing", "html_url"=>"www.yahoo.com"},
+                    {"id"=>4, "name"=>"Alta Vista", "html_url"=>"www.altavista.com"},
+                    {"id"=>5, "name"=>"Search", "html_url"=>"www.search.com"},
+                    {"id"=>6, "name"=>"Imgur", "html_url"=>"www.imgur.com"},
+                    {"id"=>7, "name"=>"Ask Jeeves", "html_url"=>"www.askjeeves.com"}]
 
-    allow_any_instance_of(GithubResults).to receive(:call_github_service).and_return(stubbed_repos)
+    allow_any_instance_of(GithubResults).to receive(:call_github_service).and_call_original
+    allow_any_instance_of(GithubResults).to receive(:call_github_service).with('repos', user).and_return(stubbed_repos)
 
     visit '/'
     click_on "Sign In"
@@ -55,6 +56,7 @@ describe 'A registered user' do
       expect(page).to have_link("ALTA VISTA")
       expect(page).to have_link("SEARCH")
       expect(page).to_not have_link("IMGUR")
+      expect(page).to_not have_link("ASK JEEVES")
     end
   end
 end
