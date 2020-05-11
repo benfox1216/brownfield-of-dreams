23ef4766e5c88e3df438e3732ad28a4339576937
Ben Fox <benfox1216@gmail.com>
2020-05-11 12:01:55 -0600

    Display error message when visitor tries to bookmark tutorial

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -3,12 +3,14 @@ class UserVideosController < ApplicationController
 
   def create
     user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
+    if !current_user
+      flash[:error] = "User must login to bookmark videos."
+    elsif current_user.user_videos.find_by(video_id: user_video.video_id)
       flash[:error] = 'Already in your bookmarks'
     elsif user_video.save
       flash[:success] = 'Bookmark added to your dashboard!'
     end
-
+    
     redirect_back(fallback_location: root_path)
   end
 
