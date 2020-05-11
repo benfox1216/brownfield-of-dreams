c79846bfdda9ab4920050d87376f68fc97dfeb22
Andrew Reid <areid.sj@gmail.com>
2020-05-11 07:42:15 -0600

    Write and pass test for displaying classroom content after login

--- spec/features/visitors/visitor_cannot_see_classsroom_content_spec.rb
+++ spec/features/visitors/visitor_cannot_see_classsroom_content_spec.rb
@@ -20,10 +20,26 @@ describe 'visitor cannot see classroom content ' do
 
   it 'on the homepage' do
     visit "/"
-    save_and_open_page
+
     expect(page).to have_link("Back End Engineering - Prework")
     expect(page).to have_content("Videos for prework.")
     expect(page).to_not have_link("Back End Engineer - Classroom Content")
     expect(page).to_not have_content("Seems eerily similar to prework videos.")
+  end
+
+  it 'unless they log in' do
+    user = create(:user)
+    visit '/'
+    click_on "Sign In"
+    fill_in 'session[email]', with: user.email
+    fill_in 'session[password]', with: user.password
+    click_on 'Log In'
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/'
+
+    expect(page).to have_link("Back End Engineering - Prework")
+    expect(page).to have_content("Videos for prework.")
+    expect(page).to have_link("Back End Engineer - Classroom Content")
+    expect(page).to have_content("Seems eerily similar to prework videos.")
   end
 end
