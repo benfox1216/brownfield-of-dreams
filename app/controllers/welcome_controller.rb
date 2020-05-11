4bae362011d781a753c74abe6b12a210b590e4cc
Andrew Reid <areid.sj@gmail.com>
2020-05-11 07:56:17 -0600

    Refactor welcome controller and tutorial model methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    @tutorials = if params[:tag]
-                   Tutorial.tagged_with(params[:tag])
-                           .paginate(page: params[:page], per_page: 5)
-                 elsif !current_user
-                   Tutorial.non_classroom_videos.paginate(page: params[:page], per_page: 5)
-                 else
-                   Tutorial.all.paginate(page: params[:page], per_page: 5)
-                 end
+    # @tutorials = if params[:tag]
+    #                Tutorial.tagged_with(params[:tag])
+    #                        .paginate(page: params[:page], per_page: 5)
+    #              elsif !current_user
+    #                Tutorial.non_classroom_videos.paginate(page: params[:page], per_page: 5)
+    #              else
+    #                Tutorial.all.paginate(page: params[:page], per_page: 5)
+    #              end
+    @tutorials = Tutorial.get_welcome_videos(params[:tag], params[:page], current_user)
+
   end
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -3,7 +3,29 @@ class Tutorial < ApplicationRecord
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
 
-  def self.non_classroom_videos
-    Tutorial.all.where("classroom = false")
+  # def self.non_classroom_videos
+  #   Tutorial.all.where("classroom = false")
+  # end
+
+  def self.get_welcome_videos(tag, page, user)
+    if tag
+      self.param_tagged_videos(tag, page)
+    elsif !user
+      self.non_classroom_videos(page)
+    else
+      self.all_videos(page)
+    end
+  end
+
+  def self.non_classroom_videos(page)
+    Tutorial.all.where("classroom = false").paginate(page: page, per_page: 5)
+  end
+
+  def self.param_tagged_videos(tag, page)
+    Tutorial.tagged_with(tag).paginate(page: page, per_page: 5)
+  end
+
+  def self.all_videos(page)
+    Tutorial.all.paginate(page: page, per_page: 5)
   end
 end
