9ec5ba4e5b0faab9e13f4ee3c74ec983b1d0e75c
Andrew Reid <areid.sj@gmail.com>
2020-05-11 07:31:41 -0600

    Write and pass test to hide classroom content from visitors not logged in

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,8 @@ class Tutorial < ApplicationRecord
   has_many :videos, -> { order(position: :ASC) }, inverse_of: :tutorial
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  def self.non_classroom_videos
+    Tutorial.all.where("classroom = false")
+  end
 end
---
+++ spec/features/visitors/visitor_cannot_see_classsroom_content_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'visitor cannot see classroom content ' do
+  before (:each) do
+    @tutorial_1 = Tutorial.create(
+      title: "Back End Engineering - Prework",
+      description: "Videos for prework.",
+      thumbnail: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg",
+      playlist_id: "PL1Y67f0xPzdN6C-LPuTQ5yzlBoz2joWa5",
+      classroom: false
+    )
+    @tutorial_2 = Tutorial.create(
+      title: "Back End Engineer - Classroom Content",
+      description: "Seems eerily similar to prework videos.",
+      thumbnail: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg",
+      playlist_id: "PL1Y67f0xPzdN6C-LPuTQ5yzlBoz2joWa5",
+      classroom: true
+    )
+  end
+
+  it 'on the homepage' do
+    visit "/"
+    save_and_open_page
+    expect(page).to have_link("Back End Engineering - Prework")
+    expect(page).to have_content("Videos for prework.")
+    expect(page).to_not have_link("Back End Engineer - Classroom Content")
+    expect(page).to_not have_content("Seems eerily similar to prework videos.")
+  end
+end
