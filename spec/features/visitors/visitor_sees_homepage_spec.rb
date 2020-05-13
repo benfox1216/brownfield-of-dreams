0f2702d274d7ca33a365d52fdccced66b2f24472
Andrew Reid <areid.sj@gmail.com>
2020-05-13 16:13:19 -0600

    Add test coverage for get started page

--- spec/features/visitors/visitor_sees_homepage_spec.rb
+++ spec/features/visitors/visitor_sees_homepage_spec.rb
@@ -30,4 +30,14 @@ describe 'Visitor' do
       expect(page).to have_content("This application is designed to pull in youtube information to populate tutorials from Turing School of Software and Design's youtube channel. It's designed for anyone learning how to code, with additional features for current students.")
     end
   end
+
+  describe 'on the get started page' do
+    it 'can see some info' do
+      visit '/get_started'
+      expect(page).to have_content("Browse tutorials from the homepage.")
+      expect(page).to have_content("Filter results by selecting a filter on the side bar of the homepage.")
+      expect(page).to have_content("Register to bookmark segments.")
+      expect(page).to have_content("Sign in with census if you are a current student for addition content.")
+    end
+  end
 end
