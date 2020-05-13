fde06bb02426608dc39083d35ca40e945bd89e5d
Andrew Reid <areid.sj@gmail.com>
2020-05-13 16:11:07 -0600

    Add test for about page

--- spec/features/visitors/visitor_sees_homepage_spec.rb
+++ spec/features/visitors/visitor_sees_homepage_spec.rb
@@ -23,4 +23,11 @@ describe 'Visitor' do
       end
     end
   end
+
+  describe 'on the about page' do
+    it 'can see some info' do
+      visit '/about'
+      expect(page).to have_content("This application is designed to pull in youtube information to populate tutorials from Turing School of Software and Design's youtube channel. It's designed for anyone learning how to code, with additional features for current students.")
+    end
+  end
 end
