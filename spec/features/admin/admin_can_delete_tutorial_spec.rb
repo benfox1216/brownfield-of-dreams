a5c5a9f765d3b3b5add36b50302448b0691c3960
Ben Fox <benfox1216@gmail.com>
2020-05-13 02:15:43 -0600

    Deleting a tutorial will delete its videos as well

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -17,4 +17,27 @@ feature "An admin can delete a tutorial" do
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
+  
+  scenario "and its videos should not longer exist", :vcr do
+    admin = create(:admin)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/admin/tutorials/new'
+    click_link 'Import YouTube Playlist'
+    fill_in 'Title', with: 'Animals Making Football Predictions'
+    fill_in 'Description', with: "Yeah. It's exactly what it sounds like. What a world we live in."
+    fill_in 'Playlist', with: 'PLbpi6ZahtOH5NpWaK0akzu0Pgley4eEXH'
+    click_on 'Save'
+    
+    expect(current_path).to eq('/admin/dashboard')
+    expect(Video.last.tutorial_id).to eq(Tutorial.last.id)
+    
+    within(first('.admin-tutorial-card')) do
+      click_link 'Delete'
+    end
+
+    expect(Tutorial.last).to eq(nil)
+    expect(Video.last).to eq(nil)
+  end
 end
