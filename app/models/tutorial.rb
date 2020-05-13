3e2ef2f161ab8784c7bbe69b5d345c602a10e302
Andrew Reid <areid.sj@gmail.com>
2020-05-13 11:35:53 -0600

    Add validation to tutorial model

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,6 +2,8 @@ class Tutorial < ApplicationRecord
   has_many :videos, -> { order(position: :ASC) },
            inverse_of: :tutorial,
            dependent: :destroy
+  validates :title, presence: true
+  validates :description, presence: true
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
 
--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:description)}
+  end
+
   describe 'relationships' do
     it { should have_many(:videos).dependent(:destroy)}
   end
