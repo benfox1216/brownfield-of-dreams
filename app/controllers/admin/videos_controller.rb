c3fb334805e363cf2a8b765abd2a3645d0d87df6
Andrew Reid <areid.sj@gmail.com>
2020-05-14 06:59:02 -0600

    Refactor videos controller

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,15 +1,12 @@
 class Admin::VideosController < Admin::BaseController
   def create
-    begin
-      tutorial = Tutorial.find(params[:tutorial_id])
-      thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
-      video = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
+    tutorial = Tutorial.find(params[:tutorial_id])
+    thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
+    video = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
 
-      video.save
-
+    if video.save
       flash[:success] = 'Successfully created video.'
-    rescue StandardError
-      # We should get more specific instead of swallowing all errors.
+    else
       flash[:error] = 'Unable to create video.'
     end
 
@@ -17,10 +14,6 @@ class Admin::VideosController < Admin::BaseController
   end
 
   private
-
-  def video_params
-    params.permit(:position)
-  end
 
   def new_video_params
     params.require(:video).permit(:title, :description, :video_id, :thumbnail)
