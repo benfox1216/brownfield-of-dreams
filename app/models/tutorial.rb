d648683156e3be384dbee13ec207b1426b16639c
Ben Fox <benfox1216@gmail.com>
2020-05-14 01:19:51 -0600

    Fix rubocop errors

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -28,12 +28,10 @@ class Tutorial < ApplicationRecord
   def self.all_videos(page)
     Tutorial.all.paginate(page: page, per_page: 5)
   end
-  
+
   def find_user_videos
     videos.map do |video|
-      if video.user_videos
-        video
-      end
+      video if video.user_videos
     end
   end
 end
--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,7 +1,7 @@
 class UserVideo < ApplicationRecord
   belongs_to :video
   belongs_to :user
-  
+
   def self.tutorials_with_videos
     all.map do |user_video|
       Tutorial.find(user_video.video.tutorial_id)
